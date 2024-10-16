758f0fc0402733e95427e36316a8282df0a83000
mohamedelsaid056 <eng.mohamedelsaid1@gmail.com>
2024-10-16 18:12:11 +0300

    16-10

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        // if you want to use auto increment id in database 
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        // if you want to use not auto increment id in database, for you need to enter id manually
+        // use [databaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
