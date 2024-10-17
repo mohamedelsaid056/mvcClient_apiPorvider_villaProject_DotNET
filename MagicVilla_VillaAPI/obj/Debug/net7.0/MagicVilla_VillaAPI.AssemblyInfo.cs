007b14c70f5ddfa52b2bc254652392a5e3863800
mohamedelsaid056 <eng.mohamedelsaid1@gmail.com>
2024-10-17 11:01:37 +0300

    start with part 2 17-10
    start with part 2 17-10

--- MagicVilla_VillaAPI/obj/Debug/net7.0/MagicVilla_VillaAPI.AssemblyInfo.cs
+++ MagicVilla_VillaAPI/obj/Debug/net7.0/MagicVilla_VillaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758f0fc0402733e95427e36316a8282df0a83000")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
